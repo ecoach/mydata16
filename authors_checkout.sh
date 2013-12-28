27a39a9daa6d0b31d4bb63bfb98c72710c96efac
jaredtritz <jtritz@umich.edu>
2013-12-27 21:20:23 -0500

    missed this, but checkout script is project specific

--- authors_checkout.sh
+++ authors_checkout.sh
@@ -3,12 +3,12 @@
 #export SCRIPT_DIR=$(pwd)
 
 echo 'begin script' 
-cd ~jtritz/bitbucket/ecoach_webapps/mydata6/mts6
+cd ~jtritz/bitbucket/ecoach_webapps/mydata16/mts16
 svn update
 #svn update -r1424 mts.dictionary
 #svn update -r1636 mts.dictionary
 source ~jtritz/virtualenv/v1/bin/activate
-python ~jtritz/bitbucket/ecoach_webapps/manage.py collectstatic --noinput --settings=mydata6.settings
+python ~jtritz/bitbucket/ecoach_webapps/manage.py collectstatic --noinput --settings=mydata16.settings
 echo 'end of script'
 
 
